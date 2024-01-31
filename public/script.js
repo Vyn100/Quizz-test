d715ed245792966645d733e0ba40049ab9e008d5
unknown <cafedeca1702@gmail.com>
2024-01-31 22:29:10 +0100

    Ajout de commentaires sur la page script.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,23 @@
+// Attend que le document HTML soit entièrement chargé
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Récupère l'élément du formulaire de connexion
     const loginForm = document.getElementById('loginForm');
+
+    // Récupère l'élément pour afficher les messages d'erreur
     const errorMessageDiv = document.getElementById('error-message');
 
+    // Ajoute un écouteur d'événements pour le soumission du formulaire
     loginForm.addEventListener('submit', async function (event) {
+
+        // Empêche le comportement par défaut du formulaire qui rechargerait la page
         event.preventDefault();
+
+        // Récupère les valeurs des champs email et password
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
+        // Effectue une requête POST vers le serveur pour vérifier les informations de connexion
         const response = await fetch('/login', {
             method: 'POST',
             headers: {
@@ -15,11 +26,14 @@ document.addEventListener('DOMContentLoaded', () => {
             body: JSON.stringify({ email, password })
         });
 
+        // Si la réponse n'est pas OK (statut 200), affiche le message d'erreur
         if (!response.ok) {
             const result = await response.json();
-            errorMessageDiv.textContent = result.error; // Affiche le message d'erreur
+            errorMessageDiv.textContent = result.error;
         } else {
-            window.location.href = '/quizz'; // Redirige vers la page de quizz
+
+            // Si la connexion réussit, redirige l'utilisateur vers la page du quiz
+            window.location.href = '/quizz';
         }
     });
 });
